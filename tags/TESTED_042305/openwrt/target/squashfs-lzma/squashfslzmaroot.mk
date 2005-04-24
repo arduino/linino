33d102ff524e2ea09defd667c2efe8bd54b3989b
(no author) <(no author)@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2005-04-24 05:24:03 +0000

    This commit was manufactured by cvs2svn to create tag 'TESTED_042305'.
    git-svn-id: svn://svn.openwrt.org/openwrt@710 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ tags/TESTED_042305/openwrt/target/squashfs-lzma/squashfslzmaroot.mk
@@ -0,0 +1,45 @@
+#############################################################
+#
+# squashfs target 
+# 
+#############################################################
+
+SQUASHFSLZMA_DIR=$(BUILD_DIR)/squashfs2.1-r2
+SQUASHFSLZMA_SOURCE=squashfs2.1-r2.tar.gz
+SQUASHFSLZMA_SITE=http://mesh.dl.sourceforge.net/sourceforge/squashfs/
+
+$(DL_DIR)/$(SQUASHFSLZMA_SOURCE):
+	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(SQUASHFSLZMA_SOURCE) x $(SQUASHFSLZMA_SITE)
+
+$(SQUASHFSLZMA_DIR)/.unpacked: $(DL_DIR)/$(SQUASHFSLZMA_SOURCE) 
+	zcat $(DL_DIR)/$(SQUASHFSLZMA_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	patch -d $(SQUASHFSLZMA_DIR) -p1 < squashfs2.0-tools-lzma.patch
+	touch $(SQUASHFSLZMA_DIR)/.unpacked
+
+$(SQUASHFSLZMA_DIR)/squashfs-tools/mksquashfs-lzma: $(SQUASHFSLZMA_DIR)/.unpacked
+	$(MAKE) -C $(SQUASHFSLZMA_DIR)/squashfs-tools mksquashfs-lzma LZMAPATH=$(BUILD_DIR)/lzma
+
+$(STAGING_DIR)/bin/mksquashfs-lzma: $(SQUASHFSLZMA_DIR)/squashfs-tools/mksquashfs-lzma
+	cp $< $@
+
+squashfslzma: $(SQUASHFSLZMA_DIR)/squashfs-tools/mksquashfs-lzma $(STAGING_DIR)/bin/mksquashfs-lzma
+	
+squashfslzma-source: $(DL_DIR)/$(SQUASHFSLZMA_SOURCE)
+
+squashfslzma-clean:
+	-$(MAKE) -C $(SQUASHFSLZMA_DIR)/squashfs-tools clean
+
+squashfslzma-dirclean:
+	rm -rf $(SQUASHFSLZMA_DIR)
+
+
+#############################################################
+#
+# Build the squashfs root filesystem image
+#
+#############################################################
+
+squashfslzmaroot-source: squashfslzma-source
+
+squashfslzmaroot-dirclean:
+	rm -rf $(SQUASHFSLZMA_DIR)
