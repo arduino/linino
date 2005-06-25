fa68a80be020448b739e39b8a0597519b960cd72
(no author) <(no author)@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2005-06-25 06:38:42 +0000

    This commit was manufactured by cvs2svn to create tag 'whiterussian_rc1'.
    git-svn-id: svn://svn.openwrt.org/openwrt@1284 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ tags/whiterussian_rc1/openwrt/target/linux/linux-2.4/broadcom.mk
@@ -0,0 +1,56 @@
+#############################################################
+# $Id$
+#
+# Makefile for the proprietary Broadcom drivers
+#
+#############################################################
+
+# broadcom specific kmod packages
+$(eval $(call KMOD_template,DIAG,diag,\
+	$(MODULES_DIR)/kernel/drivers/net/diag/diag.o \
+))
+$(eval $(call KMOD_template,BRCM_WL,brcm-wl,\
+	$(MODULES_DIR)/kernel/drivers/net/wl/wl.o \
+,CONFIG_WL))
+$(eval $(call KMOD_template,BRCM_ET,brcm-et,\
+	$(MODULES_DIR)/kernel/drivers/net/et/et.o \
+,CONFIG_ET))
+$(eval $(call KMOD_template,LP,lp,\
+	$(MODULES_DIR)/kernel/drivers/parport/parport.o \
+	$(MODULES_DIR)/kernel/drivers/parport/parport_splink.o \
+	$(MODULES_DIR)/kernel/drivers/char/lp.o \
+,CONFIG_PARPORT))
+
+LINUX_BINARY_DRIVER_SITE=http://openwrt.org/downloads/sources
+# proprietary driver, extracted from asus GPL sourcetree GPL_1941.zip
+LINUX_BINARY_WL_DRIVER=kernel-binary-wl-0.3.tar.gz
+LINUX_BINARY_WL_MD5SUM=cc45df670bcfb4e74a709b9d7beba636
+LINUX_ET_DRIVER=kernel-source-et-0.10.tar.gz
+LINUX_ET_MD5SUM=408901f0b3c672ea0f353795391f07f6
+
+$(DL_DIR)/$(LINUX_BINARY_WL_DRIVER):
+	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(LINUX_BINARY_WL_DRIVER) $(LINUX_BINARY_WL_MD5SUM) $(LINUX_BINARY_DRIVER_SITE)
+
+$(DL_DIR)/$(LINUX_ET_DRIVER):
+	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(LINUX_ET_DRIVER) $(LINUX_ET_MD5SUM) $(LINUX_BINARY_DRIVER_SITE)
+	
+$(LINUX_DIR)/.depend_done: $(LINUX_DIR)/.drivers-unpacked
+$(LINUX_DIR)/.modules_done: $(LINUX_DIR)/.drivers-unpacked
+
+$(LINUX_DIR)/.drivers-unpacked: $(LINUX_DIR)/.unpacked
+	-mkdir -p $(BUILD_DIR)
+	zcat $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	zcat $(DL_DIR)/$(LINUX_ET_DRIVER) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	# copy binary wlan driver
+	mkdir -p $(LINUX_DIR)/drivers/net/{et,wl}
+	cp -a $(BUILD_DIR)/wl/*.o $(LINUX_DIR)/drivers/net/wl
+	# copy proprietary et source
+	cp -a $(BUILD_DIR)/et/* $(LINUX_DIR)/drivers/net/et
+	mkdir -p $(LINUX_DIR)/arch/mips/bcm947xx/include/
+	cp -a $(BUILD_DIR)/et/*.h $(LINUX_DIR)/arch/mips/bcm947xx/include/
+	touch $@
+
+linux-dirclean: drivers-clean
+
+drivers-clean:
+	rm -rf $(BUILD_DIR)/{wl,et}
---
+++ tags/whiterussian_rc1/openwrt/target/linux/rules.mk
@@ -0,0 +1,34 @@
+define KMOD_template
+ifeq ($$(strip $(4)),)
+KDEPEND_$(1):=m
+else
+KDEPEND_$(1):=$($(4))
+endif
+
+ifeq ($$(strip $(5)),)
+IDEPEND_$(1):=kernel ($(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE))
+else
+IDEPEND_$(1):=kernel ($(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE)), $(5)
+endif
+
+PKG_$(1) := $(PACKAGE_DIR)/kmod-$(2)_$(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE)_$(ARCH).ipk
+I_$(1) := $(PKG_BUILD_DIR)/ipkg/$(2)
+
+ifeq ($$(KDEPEND_$(1)),m)
+ifneq ($(BR2_PACKAGE_KMOD_$(1)),)
+TARGETS += $$(PKG_$(1))
+endif
+ifeq ($(BR2_PACKAGE_KMOD_$(1)),y)
+INSTALL_TARGETS += $$(PKG_$(1))
+endif
+endif
+
+$$(PKG_$(1)): $(LINUX_DIR)/.modules_done
+	rm -rf $$(I_$(1))
+	mkdir -p $$(I_$(1))/lib/modules/$(LINUX_VERSION)
+	$(SCRIPT_DIR)/make-ipkg-dir.sh $$(I_$(1)) ../control/kmod-$(2).control $(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE) $(ARCH)
+	echo "Depends: $$(IDEPEND_$(1))" >> $$(I_$(1))/CONTROL/control
+	cp $(3) $$(I_$(1))/lib/modules/$(LINUX_VERSION)
+	$(IPKG_BUILD) $$(I_$(1)) $(PACKAGE_DIR)
+
+endef
