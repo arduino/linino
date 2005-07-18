7e0285bb9dc7a91f8cf467a710694aad5c2fc638
(no author) <(no author)@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2005-07-18 02:45:52 +0000

    This commit was manufactured by cvs2svn to create tag 'whiterussian_rc2'.
    git-svn-id: svn://svn.openwrt.org/openwrt@1498 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ tags/whiterussian_rc2/openwrt/target/linux/image/squashfs.mk
@@ -0,0 +1,20 @@
+squashfs-prepare:
+	$(MAKE) -C squashfs prepare
+
+squashfs-compile:
+	$(MAKE) -C squashfs compile
+	
+squashfs-clean:
+	$(MAKE) -C squashfs clean
+	rm -f $(KDIR)/root.squashfs
+
+$(KDIR)/root.squashfs:
+	@mkdir -p $(KDIR)/root/jffs
+	$(STAGING_DIR)/bin/mksquashfs-lzma $(KDIR)/root $@ -noappend -nopad -root-owned -le
+
+squashfs-install: $(KDIR)/root.squashfs
+	$(MAKE) -C $(BOARD) install KERNEL="$(KERNEL)" FS="squashfs"
+	
+prepare: squashfs-prepare
+compile: squashfs-compile
+install: squashfs-install
---
+++ tags/whiterussian_rc2/openwrt/target/linux/rules.mk
@@ -0,0 +1,40 @@
+define KMOD_template
+ifeq ($$(strip $(4)),)
+KDEPEND_$(1):=m
+else
+KDEPEND_$(1):=$($(4))
+endif
+
+ifeq ($$(strip $(5)),)
+IDEPEND_$(1):=kernel ($(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE))
+else
+IDEPEND_$(1):=kernel ($(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE)), $(5)
+endif
+
+PKG_$(1) := $(PACKAGE_DIR)/kmod-$(2)_$(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE)_$(ARCH).ipk
+I_$(1) := $(PKG_BUILD_DIR)/ipkg/$(2)
+
+ifeq ($$(KDEPEND_$(1)),m)
+ifneq ($(BR2_PACKAGE_KMOD_$(1)),)
+TARGETS += $$(PKG_$(1))
+endif
+ifeq ($(BR2_PACKAGE_KMOD_$(1)),y)
+INSTALL_TARGETS += $$(PKG_$(1))
+endif
+endif
+
+$$(PKG_$(1)): $(LINUX_DIR)/.modules_done
+	rm -rf $$(I_$(1))
+	mkdir -p $$(I_$(1))/lib/modules/$(LINUX_VERSION)
+	$(SCRIPT_DIR)/make-ipkg-dir.sh $$(I_$(1)) ../control/kmod-$(2).control $(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE) $(ARCH)
+	echo "Depends: $$(IDEPEND_$(1))" >> $$(I_$(1))/CONTROL/control
+	cp $(3) $$(I_$(1))/lib/modules/$(LINUX_VERSION)
+ifneq ($(6),)
+	mkdir -p $$(I_$(1))/etc/modules.d
+	for module in $(7); do \
+		echo $$$$module >> $$(I_$(1))/etc/modules.d/$(6)-$(2); \
+	done
+endif
+	$(IPKG_BUILD) $$(I_$(1)) $(PACKAGE_DIR)
+
+endef
