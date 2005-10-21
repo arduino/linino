5883d8f4995a585ea931a6adf0874302edfe8a21
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2005-10-21 15:11:03 +0000

    add Maintainer and Source to ipkg packages in make-ipkg-dir.sh, if not set already
    git-svn-id: svn://svn.openwrt.org/openwrt@2207 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/scripts/make-ipkg-dir.sh
+++ branches/whiterussian/openwrt/scripts/make-ipkg-dir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BASE=http://svn.openwrt.org/openwrt/branches/whiterussian/openwrt/package
 TARGET=$1
 CONTROL=$2
 VERSION=$3
@@ -6,6 +7,13 @@ ARCH=$4
 
 mkdir -p "$TARGET/CONTROL"
 grep '^[^(Version|Architecture)]' "$CONTROL" > "$TARGET/CONTROL/control"
+grep '^Maintainer' "$CONTROL" 2>&1 >/dev/null || \
+	echo "Maintainer: OpenWrt Developers Team <openwrt-devel@openwrt.org>" >> "$TARGET/CONTROL/control"
+grep '^Source' "$CONTROL" 2>&1 >/dev/null || {
+	pkgname=$(pwd | awk -F/ '{ n = split($0, p, "/"); if ((p[n - 1] == "package") && (p[n - 2] == "openwrt")) print p[n] }')
+	[ -z "$pkgname" ] && src="http://svn.openwrt.org" || src="$BASE/$pkgname"
+	echo "Source: $src" >> "$TARGET/CONTROL/control"
+}
 echo "Version: $VERSION" >> "$TARGET/CONTROL/control"
 echo "Architecture: $ARCH" >> "$TARGET/CONTROL/control"
 chmod 644 "$TARGET/CONTROL/control"
