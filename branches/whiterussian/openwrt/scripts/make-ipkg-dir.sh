32a99b18d17e88a798123ae916810d7f88631f48
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2005-10-21 20:15:15 +0000

    small changes to make-ipkg-dir.sh from trunk/
    git-svn-id: svn://svn.openwrt.org/openwrt@2215 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/scripts/make-ipkg-dir.sh
+++ branches/whiterussian/openwrt/scripts/make-ipkg-dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASE=http://svn.openwrt.org/openwrt/branches/whiterussian/openwrt/package
+BASE=http://svn.openwrt.org/openwrt/branches/whiterussian/openwrt
 TARGET=$1
 CONTROL=$2
 VERSION=$3
@@ -8,11 +8,11 @@ ARCH=$4
 mkdir -p "$TARGET/CONTROL"
 grep '^[^(Version|Architecture)]' "$CONTROL" > "$TARGET/CONTROL/control"
 grep '^Maintainer' "$CONTROL" 2>&1 >/dev/null || \
-	echo "Maintainer: OpenWrt Developers Team <openwrt-devel@openwrt.org>" >> "$TARGET/CONTROL/control"
+        echo "Maintainer: OpenWrt Developers Team <openwrt-devel@openwrt.org>" >> "$TARGET/CONTROL/control"
 grep '^Source' "$CONTROL" 2>&1 >/dev/null || {
-	pkgname=$(pwd | awk -F/ '{ n = split($0, p, "/"); if ((p[n - 1] == "package") && (p[n - 2] == "openwrt")) print p[n] }')
-	[ -z "$pkgname" ] && src="http://svn.openwrt.org" || src="$BASE/$pkgname"
-	echo "Source: $src" >> "$TARGET/CONTROL/control"
+        pkgbase=$(pwd | sed -e "s|^$TOPDIR/||g")
+        [ "$pkgbase" = "$WD" ] && src="N/A" || src="$BASE/$pkgbase"
+        echo "Source: $src" >> "$TARGET/CONTROL/control"
 }
 echo "Version: $VERSION" >> "$TARGET/CONTROL/control"
 echo "Architecture: $ARCH" >> "$TARGET/CONTROL/control"
