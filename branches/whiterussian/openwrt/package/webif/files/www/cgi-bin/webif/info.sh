cca9bcfc486b6b18f8e72f52372ad2fa8d224b54
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2005-10-22 23:18:10 +0000

    fix version display in info.sh
    git-svn-id: svn://svn.openwrt.org/openwrt@2270 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/package/webif/files/www/cgi-bin/webif/info.sh
+++ branches/whiterussian/openwrt/package/webif/files/www/cgi-bin/webif/info.sh
@@ -2,35 +2,43 @@
 <? 
 . /usr/lib/webif/webif.sh
 header "Info" "Router Info" "Router Info"
+
 ?>
 <pre><?
-
+_version=$( grep "(" /etc/banner )
+_version="${_version%% ---*}"
+_kversion="$( cat /proc/version )"
+_date="$(date)"
+_mac="$(/sbin/ifconfig eth0 | grep HWaddr | cut -b39-)"
 sed -e 's/&/&amp;/g' < /etc/banner
-
-?></pre>
+cat <<EOF
+</pre>
 <br />
 <br />
 <table style="width: 90%; text-align: left;" border="0" cellpadding="2" cellspacing="2" align="center">
 <tbody>
 	<tr>
 		<td>Firmware Version</td>
-		<td><? grep "(" /etc/banner | cut -b-20 | cut -b2- ?></td>
+		<td>$_version</td>
 	</tr>
 	<tr>
 		<td>Kernel Version</td>
-		<td><? cat /proc/version ?></td>
+		<td>$_kversion</td>
 	</tr>
 	<tr>
 		<td>Current Date/Time</td>
-		<td><? date ?></td>
+		<td>$_date</td>
 	</tr>
 	<tr>
 		<td>MAC-Address</td>
-		<td><? /sbin/ifconfig eth0 | grep HWaddr | cut -b39-  ?></td>
+		<td>$_mac</td>
 	</tr>
 </tbody>
 </table>
-<? footer ?>
+EOF
+
+footer
+?>
 <!--
 ##WEBIF:name:Info:2:Router Info
 -->
