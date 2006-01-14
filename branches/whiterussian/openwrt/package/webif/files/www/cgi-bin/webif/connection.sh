350abd11bab3d908cada63fd26095246cb02e50b
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2006-01-14 14:43:34 +0000

    fix open tag and weird html structure in webif connections page, suppress  missing AF_INET6  messages
    git-svn-id: svn://svn.openwrt.org/openwrt@2974 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/package/webif/files/www/cgi-bin/webif/connection.sh
+++ branches/whiterussian/openwrt/package/webif/files/www/cgi-bin/webif/connection.sh
@@ -6,28 +6,22 @@ header "Status" "Connections" "Connection status"
 <table style="width: 90%; text-align: left;" border="0" cellpadding="2" cellspacing="2" align="center">
 <tbody>
 	<tr>
-		<td><b>Ethernet/Wireless physical connections</b></td>
-		<tbody>
-			<tr>
-				<td><pre><? cat /proc/net/arp | head -1 ?></pre></td>
-			</tr>
-			<tr>
-				<td><pre><? cat /proc/net/arp | grep -v IP ?></pre></td>
-			</tr>
-		</tbody>
+		<th><b>Ethernet/Wireless physical connections</b></th>
 	</tr>
 	<tr>
-		<td><br /><br /></td>
+		<td><pre><? cat /proc/net/arp ?></pre></td>
+	</tr>
+	
+	<tr><td><br /><br /></td></tr>
+
+	<tr>
+		<th><b>Connections to the router</b></th>
 	</tr>
 	<tr>
-		<td><b>Connections to the router</b></td>
-		<tbody>
-			<tr>
-				<td><pre><? netstat -n ?></pre></td>
-			</tr>
-		</tbody>
+		<td><pre><? netstat -n 2>&- ?></pre></td>
 	</tr>
 </tbody>
+</table>
 
 <? footer ?>
 <!--
