b7cfdb5fd2db873a624f2c2d57ff5214e69c33b9
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2006-02-05 18:46:09 +0000

    add support for overriding the encoding in webif languages
    git-svn-id: svn://svn.openwrt.org/openwrt@3146 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/package/webif/files/usr/lib/webif/webif.sh
+++ branches/whiterussian/openwrt/package/webif/files/usr/lib/webif/webif.sh
@@ -83,11 +83,13 @@ header() {
 Content-Type: text/html
 Pragma: no-cache
 
+<?xml version="1.0" encoding="@TR<<Encoding|ISO-8859-1>>"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
     	<title>@TR<<OpenWrt Administrative Console>></title>
 		<link rel="stylesheet" type="text/css" href="/webif.css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=@TR<<Encoding|ISO-8859-1>>" />
 	</head>
 	<body $4><div id="container">
 	    <div id="header">
