337880fa29ece4890375021cb9dbe206a025a034
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2006-02-06 02:37:42 +0000

    add webif language changer to system settings
    git-svn-id: svn://svn.openwrt.org/openwrt@3157 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/package/webif/files/www/cgi-bin/webif/system.sh
+++ branches/whiterussian/openwrt/package/webif/files/www/cgi-bin/webif/system.sh
@@ -3,13 +3,16 @@
 . /usr/lib/webif/webif.sh
 load_settings system
 load_settings nvram
+load_settings webif
 
 if empty "$FORM_submit"; then
-	FORM_hostname=${wan_hostname:-$(nvram get wan_hostname)}
-	FORM_hostname=${FORM_hostname:-OpenWrt}
+	FORM_hostname="${wan_hostname:-$(nvram get wan_hostname)}"
+	FORM_hostname="${FORM_hostname:-OpenWrt}"
+	FORM_language="${language:-$(nvram get language)}"
+	FORM_language="${FORM_language:-default}"
 	is_bcm947xx && {
-		FORM_boot_wait=${boot_wait:-$(nvram get boot_wait)}
-		FORM_boot_wait=${FORM_boot_wait:-off}
+		FORM_boot_wait="${boot_wait:-$(nvram get boot_wait)}"
+		FORM_boot_wait="${FORM_boot_wait:-off}"
 	}
 else
 	SAVED=1
@@ -17,14 +20,17 @@ else
 hostname|FORM_hostname|Hostname|nodots required|$FORM_hostname
 EOF
 	equal "$?" 0 && {
-		save_setting system wan_hostname $FORM_hostname
+		save_setting system wan_hostname "$FORM_hostname"
+		save_setting webif language "$FORM_language"
 		is_bcm947xx && {
 			case "$FORM_boot_wait" in
-				on|off) save_setting nvram boot_wait $FORM_boot_wait;;
+				on|off) save_setting nvram boot_wait "$FORM_boot_wait";;
 			esac
 		}
 	}
 fi
+
+LANGUAGES="$(grep '^[\t ]*lang[\t ]*=>' /usr/lib/webif/lang/*/*.txt | awk -f /usr/lib/webif/languages.awk)"
 
 header "System" "Settings" "@TR<<System Settings>>" '' "$SCRIPT_NAME"
 
@@ -38,7 +44,9 @@ start_form|@TR<<System Settings>>
 field|@TR<<Host Name>>
 text|hostname|$FORM_hostname
 $bootwait_form
-field
+field|Language
+select|language|$FORM_language
+$LANGUAGES
 end_form
 EOF
 
