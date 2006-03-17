1283c0e255907b900d29d823e29cf3781919e593
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2006-03-17 09:51:42 +0000

    clean up system code, finally remove et
    git-svn-id: svn://svn.openwrt.org/openwrt@3388 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/target/linux/linux-2.4/broadcom.mk
+++ branches/whiterussian/openwrt/target/linux/linux-2.4/broadcom.mk
@@ -12,9 +12,6 @@ $(eval $(call KMOD_template,DIAG,diag,\
 $(eval $(call KMOD_template,BRCM_WL,brcm-wl,\
 	$(MODULES_DIR)/kernel/drivers/net/wl/wl.o \
 ,CONFIG_WL))
-$(eval $(call KMOD_template,BRCM_ET,brcm-et,\
-	$(MODULES_DIR)/kernel/drivers/net/et/et.o \
-,CONFIG_ET))
 $(eval $(call KMOD_template,LP,lp,\
 	$(MODULES_DIR)/kernel/drivers/parport/parport.o \
 	$(MODULES_DIR)/kernel/drivers/parport/parport_splink.o \
@@ -26,32 +23,22 @@ LINUX_BINARY_DRIVER_SITE=http://openwrt.org/downloads/sources
 # proprietary driver, extracted from Linksys GPL sourcetree WRT54GS 4.70.6
 LINUX_BINARY_WL_DRIVER=kernel-binary-wl-0.4.tar.gz
 LINUX_BINARY_WL_MD5SUM=0659fa8f1805be6ec03188ef8e1216cc
-LINUX_ET_DRIVER=kernel-source-et-0.11.tar.gz
-LINUX_ET_MD5SUM=bdc23ab59440793e35cab039457f6358
 
 $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER):
 	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(LINUX_BINARY_WL_DRIVER) $(LINUX_BINARY_WL_MD5SUM) $(LINUX_BINARY_DRIVER_SITE)
 
-$(DL_DIR)/$(LINUX_ET_DRIVER):
-	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(LINUX_ET_DRIVER) $(LINUX_ET_MD5SUM) $(LINUX_BINARY_DRIVER_SITE)
-	
 $(LINUX_DIR)/.depend_done: $(LINUX_DIR)/.drivers-unpacked
 $(LINUX_DIR)/.modules_done: $(LINUX_DIR)/.drivers-unpacked
 
-$(LINUX_DIR)/.drivers-unpacked: $(LINUX_DIR)/.unpacked $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER) $(DL_DIR)/$(LINUX_ET_DRIVER)
+$(LINUX_DIR)/.drivers-unpacked: $(LINUX_DIR)/.unpacked $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER)
 	-mkdir -p $(BUILD_DIR)
 	zcat $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	zcat $(DL_DIR)/$(LINUX_ET_DRIVER) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	# copy binary wlan driver
-	mkdir -p $(LINUX_DIR)/drivers/net/{et,wl}
+	mkdir -p $(LINUX_DIR)/drivers/net/wl
 	cp -a $(BUILD_DIR)/wl/*.o $(LINUX_DIR)/drivers/net/wl
-	# copy proprietary et source
-	cp -a $(BUILD_DIR)/et/* $(LINUX_DIR)/drivers/net/et
-	mkdir -p $(LINUX_DIR)/arch/mips/bcm947xx/include/
-	cp -a $(BUILD_DIR)/et/*.h $(LINUX_DIR)/arch/mips/bcm947xx/include/
 	touch $@
 
 linux-dirclean: drivers-clean
 
 drivers-clean:
-	rm -rf $(BUILD_DIR)/{wl,et}
+	rm -rf $(BUILD_DIR)/wl
