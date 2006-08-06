d5ff254c00a6a739bdeb7a5e496bfeee3ec303f7
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2006-08-06 23:16:21 +0000

    add config parsing functions from buildroot-ng to whiterussian
    git-svn-id: svn://svn.openwrt.org/openwrt@4492 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/package/base-files/default/etc/functions.sh
+++ branches/whiterussian/openwrt/package/base-files/default/etc/functions.sh
@@ -1,4 +1,12 @@
-#!/bin/ash
+#!/bin/sh
+# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006 Fokus Fraunhofer <carsten.tittel@fokus.fraunhofer.de>
+
+# newline
+N="
+"
+
+_C=0
 
 alias debug=${DEBUG:-:}
 
@@ -87,4 +95,77 @@ do_ifup() {
 		echo "### ifup ${2}: ignored ${2}_proto=\"$1\" (not supported)"
 	;;
 	esac
+}
+
+append() {
+	local var="$1"
+	local value="$2"
+	local sep="${3:- }"
+	eval "export ${var}=\"\${${var}:+\${${var}}${value:+$sep}}$value\""
+}
+
+reset_cb() {
+	config_cb() {
+		return 0
+	}
+	option_cb() {
+		return 0
+	}
+}
+reset_cb
+
+config () {
+    local cfgtype="$1"
+    local name="$2"
+    _C=$(($_C + 1))
+    name="${name:-cfg${_C}}"
+    config_cb "$cfgtype" "$name"
+    export CONFIG_SECTION="$name"
+    export CONFIG_${CONFIG_SECTION}_TYPE="$cfgtype"
+}
+
+option () {
+	local varname="$1"; shift
+	export CONFIG_${CONFIG_SECTION}_${varname}="$*"
+	option_cb "$varname" "$*"
+}
+
+config_clear() {
+	[ -z "$CONFIG_SECTION" ] && return
+	for oldsetting in `set | grep ^CONFIG_${CONFIG_SECTION}_ | \
+		sed -e 's/\(.*\)=.*$/\1/'` ; do 
+		unset $oldsetting 
+	done
+	unset CONFIG_SECTION
+}
+
+config_load() {
+	local DIR="./"
+	_C=0
+	[ \! -e "$1" -a -e "/etc/config/$1" ] && {
+		DIR="/etc/config/"
+	}
+	[ -e "$DIR$1" ] && {
+		CONFIG_FILENAME="$DIR$1"
+		. ${CONFIG_FILENAME}
+	} || return 1
+	${CD:+cd -} >/dev/null
+	${CONFIG_SECTION:+config_cb}
+}
+
+config_get() {
+	case "$3" in
+		"") eval "echo \${CONFIG_${1}_${2}}";;
+		*) eval "$1=\"\${CONFIG_${2}_${3}}\"";;
+	esac
+}
+
+config_set() {
+	export CONFIG_${1}_${2}="${3}"
+}
+
+include() {
+	for file in $(ls /lib/$1/*.sh 2>/dev/null); do
+		. $file
+	done
 }
