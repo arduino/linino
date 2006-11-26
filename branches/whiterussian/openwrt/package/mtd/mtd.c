8d2e1cad3744df63bc9ee14db5cb63ad8c687bf9
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2006-11-26 01:03:21 +0000

    add reboot fix for mtd (thanks, wbx)
    git-svn-id: svn://svn.openwrt.org/openwrt@5645 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/whiterussian/openwrt/package/mtd/mtd.c
+++ branches/whiterussian/openwrt/package/mtd/mtd.c
@@ -28,6 +28,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <error.h>
@@ -41,6 +43,7 @@
 #include <string.h>
 
 #include <linux/mtd/mtd.h>
+#include <linux/reboot.h>
 
 #define TRX_MAGIC       0x30524448      /* "HDR0" */
 #define BUFSIZE (16 * 1024)
@@ -473,8 +476,9 @@ int main (int argc, char **argv)
 
 	sync();
 	
-	if (boot)
-		kill(1, 15); // send SIGTERM to init for reboot
-
+	if (boot) {
+		fflush(stdout);
+		syscall(SYS_reboot,LINUX_REBOOT_MAGIC1,LINUX_REBOOT_MAGIC2,LINUX_REBOOT_CMD_RESTART,NULL);
+	}
 	return 0;
 }
