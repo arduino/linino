4536af37dac80e557626440841d0ab18b4aeb283
nico <nico@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2009-04-10 13:44:15 +0000

    merge r13988 to 8.09 (closes: #4742)
    git-svn-id: svn://svn.openwrt.org/openwrt@15202 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/8.09/target/linux/x86/generic/profiles/000-Generic.mk
+++ branches/8.09/target/linux/x86/generic/profiles/000-Generic.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006-2009 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -7,7 +7,10 @@
 
 define Profile/Generic
   NAME:=Generic
-  PACKAGES:=
+  PACKAGES:= \
+	kmod-3c59x kmod-8139too kmod-e100 kmod-e1000 kmod-natsemi \
+	kmod-ne2k-pci kmod-pcnet32 kmod-r8169 kmod-sis900 kmod-sky2 \
+	kmod-tg3 kmod-via-rhine
 endef
 
 define Profile/Generic/Description
