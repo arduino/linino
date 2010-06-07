be0ec8f6dad15544754d64e4a231d6bbfca98105
jow <jow@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2010-06-07 10:48:03 +0000

    [backfire] merge r21700
    git-svn-id: svn://svn.openwrt.org/openwrt@21701 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- branches/backfire/package/kernel/modules/netfilter.mk
+++ branches/backfire/package/kernel/modules/netfilter.mk
@@ -236,7 +236,7 @@ $(call KernelPackage/ipt/Depends,)
   FILES:= \
 	$(LINUX_DIR)/drivers/net/imq.$(LINUX_KMOD_SUFFIX) \
 	$(foreach mod,$(IPT_IMQ-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
-  AUTOLOAD:=$(call AutoLoad,45,$(notdir \
+  AUTOLOAD:=$(call AutoLoad,46,$(notdir \
 	imq \
 	$(IPT_IMQ-m) \
   ))
