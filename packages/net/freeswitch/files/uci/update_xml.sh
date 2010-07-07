4b2215cf1157ba35b4f0e69cbbd3fad172f9a58d
cshore <cshore@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2010-07-07 11:18:11 +0000

    [packages]: net/freeswitch: Added initial uci configuration capability.
    git-svn-id: svn://svn.openwrt.org/openwrt@22084 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ packages/net/freeswitch/files/uci/update_xml.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright (C) 2010 Vertical Communications
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+
+# Note that the fs_profile_* functions require the FS XML files contains
+# commented out sections for the parameters that are not currently in use, but
+# which are to be available to the UCI FS config
+fs_init_xml() {
+	config_load freeswitch
+	fs_profile_internal_top "internal_top" "/etc/freeswitch/sip_profiles/internal.xml"
+	fs_profile_external_top "external_top" "/etc/freeswitch/sip_profiles/external.xml"
+	fs_profile_gateway "external_example" "/etc/freeswitch/sip_profiles/external/example.xml"
+	fs_profile_gateway "internal_example" "/etc/freeswitch/sip_profiles/internal/example.xml"
+}
