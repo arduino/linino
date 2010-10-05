7ab8bbc7bc6ca1b91d46d0aaa7bdc761a7c20b2e
nunojpg <nunojpg@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2010-10-05 21:51:46 +0000

    [packages] sshtunnel: update to version 2
    git-svn-id: svn://svn.openwrt.org/openwrt@23263 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- packages/net/sshtunnel/files/usr/bin/sshtunnel.sh
+++ packages/net/sshtunnel/files/usr/bin/sshtunnel.sh
@@ -2,18 +2,19 @@
 
 PIDFILE="/tmp/run/sshtunnel"
 
-args=$1
-retrydelay=$2
+args="$1"
+retrydelay="$2"
+server="$3"
 
 while true
 do
-	logger -p daemon.info -t "sshtunnel[$$]" "connecting: ssh $args"
+	logger -p daemon.info -t "sshtunnel[$$][$server]" "connection started"
 	
 	start-stop-daemon -S -p "$PIDFILE"_"$$".pid -mx ssh -- $args &>/tmp/log/sshtunnel_$$ 
-	logger -p daemon.err -t "sshtunnel[$$]" < /tmp/log/sshtunnel_$$
-	rm /tmp/log/sshtunnel_$$
 	
-	logger -p daemon.info -t "sshtunnel[$$]" "ssh exited with code $?, retrying in $retrydelay seconds"
+	logger -p daemon.err -t "sshtunnel[$$][$server]" < /tmp/log/sshtunnel_$$
+	rm /tmp/log/sshtunnel_$$
+	logger -p daemon.info -t "sshtunnel[$$][$server]" "ssh exited with code $?, retrying in $retrydelay seconds"
 	
 	sleep "$retrydelay" & wait
 done
