cc36f7dacba5095696045eb177ee2f1a6582fa66
nunojpg <nunojpg@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2010-12-22 23:14:01 +0000

    [packages] sshtunnel: bug fix
    git-svn-id: svn://svn.openwrt.org/openwrt@24807 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- packages/net/sshtunnel/files/sshtunnel.sh
+++ packages/net/sshtunnel/files/sshtunnel.sh
@@ -10,11 +10,12 @@ while true
 do
 	logger -p daemon.info -t "sshtunnel[$$][$server]" "connection started"
 	
-	start-stop-daemon -S -p "$PIDFILE"_"$$".pid -mx ssh -- $args &>/tmp/log/sshtunnel_$$ 
+	start-stop-daemon -S -p "${PIDFILE}_${$}.pid" -mx ssh -- $args &>/tmp/log/sshtunnel_$$ 
 	
 	logger -p daemon.err -t "sshtunnel[$$][$server]" < /tmp/log/sshtunnel_$$
 	rm /tmp/log/sshtunnel_$$
 	logger -p daemon.info -t "sshtunnel[$$][$server]" "ssh exited with code $?, retrying in $retrydelay seconds"
-	
+	rm "${PIDFILE}_${$}.pid"
+
 	sleep "$retrydelay" & wait
 done
