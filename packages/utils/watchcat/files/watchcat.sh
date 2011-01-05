f3707d0478207ca8d4ebdf668bbd078d9fa2ef83
nunojpg <nunojpg@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2011-01-05 15:33:39 +0000

    [packages] watchcat: update to release 5
    git-svn-id: svn://svn.openwrt.org/openwrt@24909 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- packages/utils/watchcat/files/watchcat.sh
+++ packages/utils/watchcat/files/watchcat.sh
@@ -6,7 +6,7 @@ shutdown_now() {
 	local forcedelay="$1"
 
 	reboot &
-	
+
 	[ "$forcedelay" -ge 1 ] && {
 		sleep "$forcedelay"
 
@@ -15,39 +15,49 @@ shutdown_now() {
 }
 
 watchcat_allways() {
-	local period="$1"; local forcedelay="$2" 
-	
+	local period="$1"; local forcedelay="$2"
+
 	sleep "$period" && shutdown_now "$forcedelay"
 }
 
 watchcat_ping() {
 	local period="$1"; local forcedelay="$2"; local pinghosts="$3"; local pingperiod="$4"
-	
+
 	time_now="$(cat /proc/uptime)"
 	time_now="${time_now%%.*}"
-	time_last="$time_now"
+	time_lastcheck="$time_now"
+	time_lastcheck_withinternet="$time_now"
 
 	while true
 	do
-		sleep "$pingperiod"
-	
+		# account for the time ping took to return. With a ping time of 5s, ping might take more than that, so it is important to avoid even more delay.
 		time_now="$(cat /proc/uptime)"
 		time_now="${time_now%%.*}"
-		
- 		for host in "$pinghosts" 
+		time_diff="$((time_now-time_lastcheck))"
+
+		[ "$time_diff" -lt "$pingperiod" ] && {
+			sleep_time="$((pingperiod-time_diff))"
+			sleep "$sleep_time"
+		}
+
+		time_now="$(cat /proc/uptime)"
+		time_now="${time_now%%.*}"
+		time_lastcheck="$time_now"
+
+		for host in "$pinghosts"
 		do
 			if ping -c 1 "$host" &> /dev/null 
 			then 
-				time_last="$time_now"
+				time_lastcheck_withinternet="$time_now"
 			else
-				time_diff="$((time_now-time_last))"
-				logger -p daemon.info -t "watchcat[$$]" "no internet connectivity for $time_diff seconds. Reseting when reaching $period"	
+				time_diff="$((time_now-time_lastcheck_withinternet))"
+				logger -p daemon.info -t "watchcat[$$]" "no internet connectivity for $time_diff seconds. Reseting when reaching $period"       
 			fi
 		done
 
-		time_diff="$((time_now-time_last))"
+		time_diff="$((time_now-time_lastcheck_withinternet))"
 		[ "$time_diff" -ge "$period" ] && shutdown_now "$forcedelay"
-	
+
 	done
 }
 
