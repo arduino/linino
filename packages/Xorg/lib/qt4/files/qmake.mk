13d2e9dd50644876418576978a83cca00d28cb7c
mirko <mirko@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2011-02-22 12:12:59 +0000

    [packages/qt4] we need to pass all the environment variables not just to $(QMAKE) but also to $(MAKE), because Makefiles may invoke qmake once again for creating further Makefiles
    git-svn-id: svn://svn.openwrt.org/openwrt@25659 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- packages/Xorg/lib/qt4/files/qmake.mk
+++ packages/Xorg/lib/qt4/files/qmake.mk
@@ -40,7 +40,21 @@ define Build/Configure/Qmake
 		$(PKG_BUILD_DIR)/$(1).pro
 endef
 
+# we need to pass everything to $(MAKE) as well, as Makefiles may invoke qmake once again for creating further Makefiles
 define Build/Compile/Default
+	TARGET_CC="$(TARGET_CROSS)gcc" \
+	TARGET_CXX="$(TARGET_CROSS)g++" \
+	TARGET_AR="$(TARGET_CROSS)ar cqs" \
+	TARGET_OBJCOPY="$(TARGET_CROSS)objcopy" \
+	TARGET_RANLIB="$(TARGET_CROSS)ranlib" \
+	TARGET_CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+	TARGET_CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+	TARGET_LDFLAGS="$(TARGET_LDFLAGS) $(EXTRA_LDFLAGS)" \
+	TARGET_INCDIRS="$(TARGET_INCDIRS)" \
+	TARGET_LIBDIRS="$(TARGET_LIBDIRS)" \
+	TARGET_LIBS="$(TARGET_LIBS)" \
+	STAGING_DIR_HOST="$(STAGING_DIR)/../host" \
+	STAGING_DIR="$(STAGING_DIR)" \
 	$(MAKE) $(PKG_JOBS) -C $(PKG_BUILD_DIR)/$(MAKE_PATH) \
 		$(1);
 endef
