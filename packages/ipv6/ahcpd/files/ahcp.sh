5e5e16543ea3bad4d6a5f1a8c789ef6bab7e8241
kerneis <kerneis@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2011-10-01 11:30:03 +0000

    [packages] ahcpd: avoid half-configured ahcpd interfaces
    git-svn-id: svn://svn.openwrt.org/openwrt@28351 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- packages/ipv6/ahcpd/files/ahcp.sh
+++ packages/ipv6/ahcpd/files/ahcp.sh
@@ -2,12 +2,13 @@ setup_interface_ahcp() {
 	local interface="$1"
 	local config="$2"
 
+	setup_interface_none "$interface" "$config"
+
 	local mode=$(uci_get_state ahcpd "@ahcpd[0]" mode "client")
 	if [ "$mode" != "client" ]; then
 		echo "Warning: ahcp ignored for $interface (mode is $mode, should be client)."
 		echo "Fix ahcp mode in /etc/config/ahcpd."
-		return
+	else
+		/etc/init.d/ahcpd restart
 	fi
-	setup_interface_none "$interface" "$config"
-	/etc/init.d/ahcpd restart
 }
