08b754cca7145c83971b83e946a23e89cc0d98d4
Mimmo <mimmo@linino.org>
2013-05-12 23:14:45 +0200

    Fix prog_console.sh. Added banner

--- trunk/files/etc/linino/prog_console.sh
+++ trunk/files/etc/linino/prog_console.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# (c)  Copyright 2013 dog hunter, LLC - All righs reserved  
+# GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+# Domenico La Fauci
+# Federico Musto
 
-echo Programming console firmware...
+echo Programming console ATMEL 32U4 firmware...
 echo 1 > /sys/class/gpio/gpio21/value
-[ -e /etc/linino/Etheris_fw.hex ] {
+[ -e /etc/linino/Etheris_fw.hex ] 
 	avrdude -c linuxgpio -v -C /etc/avrdude.conf -p m32u4 -U lfuse:w:0xFF:m -U hfuse:w:0xD8:m -U efuse:w:0xFB:m -U flash:w:/etc/linino/Etheris_fw.hex
-}
+
 echo 0 > /sys/class/gpio/gpio21/value
