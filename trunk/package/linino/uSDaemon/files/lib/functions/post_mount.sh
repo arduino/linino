9b062e916f9b8d15b565df19852f8217b7008fc0
Federico Fissore <f.fissore@arduino.cc>
2013-06-27 13:56:19 +0200

    post_mount.sh: now also links /mnt/sd/www to /www/sd

--- trunk/package/linino/uSDaemon/files/lib/functions/post_mount.sh
+++ trunk/package/linino/uSDaemon/files/lib/functions/post_mount.sh
@@ -1,22 +1,37 @@
-#!/bin/sh -ex
+#!/bin/sh
 
 SD=/mnt/sd
+WWW=/www/sd
 
-post_auto_mount() {
-	local mnt_path=$1
+_symlink_if_special() {
+	local special_folder=$1
+	local source=$2
+	local destination=$3
 
-	if [ -d "$mnt_path/arduino" ]; then
-		if [ ! -L $SD ] || [ ! -d $SD ]; then
-			ln -s $mnt_path $SD
-			logger -t "automount" "arduino folder found: $mnt_path is now available at $SD"
+	if [ -d $special_folder ]; then
+		if [ ! -L $destination ] || [ ! -d $destination ]; then
+			ln -s $source $destination
+			logger -t "automount" "$special_folder folder found: $source is now available at $destination"
 		fi
 	fi
 }
 
-post_auto_umount() {
-	if [ -L $SD ]; then
-		if [ ! -d $SD ]; then
-			rm $SD
-		fi
+_remove_symlink_if_invalid() {
+	local symlink=$1
+	if [ -L $symlink ] && [ ! -d $symlink ]; then
+		logger -t "automount" "Removing $symlink"
+		rm $symlink
 	fi
+}
+
+post_auto_mount() {
+	local mnt_path=$1
+
+	_symlink_if_special "$mnt_path/arduino" $mnt_path $SD
+	_symlink_if_special "$mnt_path/arduino/www" "$mnt_path/arduino/www" $WWW
+}
+
+post_auto_umount() {
+	_remove_symlink_if_invalid $SD
+	_remove_symlink_if_invalid $WWW
 }
