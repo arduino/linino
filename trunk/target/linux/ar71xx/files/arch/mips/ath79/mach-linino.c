cfcde7e0fdf820823f5f8355710db436c8daea79
Mimmo <mimmo@linino.org>
2013-07-04 00:07:29 +0200

    Set eth interface MAC starting from wlan MAC

--- trunk/target/linux/ar71xx/files/arch/mips/ath79/mach-linino.c
+++ trunk/target/linux/ar71xx/files/arch/mips/ath79/mach-linino.c
@@ -31,11 +31,11 @@
 #define DS_GPIO_LED_USB			1
 
 #define DS_GPIO_OE			21
-#ifndef DS2_B
-#define DS_GPIO_AVR_RESET		7
-#else
 #define DS_GPIO_AVR_RESET		18
-#endif
+
+// Maintained to have the console in the previous version of DS2 working
+#define DS_GPIO_AVR_RESET_DS2           7
+#define DS2_PREV_RESET_PIN
 
 #ifdef DS1
 #define DS_GPIO_OE2			23
@@ -43,8 +43,6 @@
 #define DS_GPIO_OE2                     22
 #define DS_GPIO_UART_ENA		23
 #define DS_GPIO_CONF_BTN		20
-#define DS_GPIO_LED_SPD			22
-#define DS_GPIO_LED_FDX			18
 #endif
 
 #define DS_KEYS_POLL_INTERVAL		20	/* msecs */
@@ -67,21 +65,6 @@ static struct gpio_led ds_leds_gpio[] __initdata = {
 		.gpio		= DS_GPIO_LED_WLAN,
 		.active_low	= 0,
 	},
-#ifndef DS1
-        {
-                .name           = "ds:green:spd",
-                .gpio           = DS_GPIO_LED_SPD,
-                .active_low     = 0,
-        },
-#ifndef DS2_B
-        {
-                .name           = "ds:green:fdx",
-                .gpio           = DS_GPIO_LED_FDX,
-                .active_low     = 0,
-        },
-#endif 
-
-#endif
 };
 
 static struct gpio_keys_button ds_gpio_keys[] __initdata = {
@@ -99,14 +82,31 @@ static struct gpio_keys_button ds_gpio_keys[] __initdata = {
 
 static void __init ds_common_setup(void)
 {
+	static u8 mac[6];
+
 	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
 	ath79_register_m25p80(NULL);
+#if 0
 	ath79_register_wmac(art + DS_CALDATA_OFFSET,
 			    art + DS_WMAC_MAC_OFFSET);
 
 	ath79_init_mac(ath79_eth0_data.mac_addr, art + DS_MAC0_OFFSET, 0);
 	ath79_init_mac(ath79_eth1_data.mac_addr, art + DS_MAC1_OFFSET, 0);
+#else
+        if (ar93xx_wmac_read_mac_address(mac)) {
+                  ath79_register_wmac(NULL, NULL);
+          } else {
+                  ath79_register_wmac(art + DS_CALDATA_OFFSET,
+                                      art + DS_WMAC_MAC_OFFSET);
+                  memcpy(mac, art + DS_WMAC_MAC_OFFSET, sizeof(mac));
+          }
 
+          mac[3] |= 0x08;
+          ath79_init_mac(ath79_eth0_data.mac_addr, mac, 0);
+
+          mac[3] &= 0xF7;
+          ath79_init_mac(ath79_eth1_data.mac_addr, mac, 0);
+#endif
 	ath79_register_mdio(0, 0x0);
 
 	/* LAN ports */
@@ -118,7 +118,7 @@ static void __init ds_common_setup(void)
 
 static void __init ds_setup(void)
 {
-#ifndef DS2_B
+#ifdef DS2_PREV_RESET_PIN
 	u32 t;
 #endif
 	ds_common_setup();
@@ -137,18 +137,18 @@ static void __init ds_setup(void)
 	ath79_gpio_function2_setup(AR933X_GPIO_FUNC2_JUMPSTART_DISABLE, 0);
 
 	printk("Setting DogStick2 GPIO\n");
-#ifndef DS2_B
+#ifdef DS2_PREV_RESET_PIN
 	t = ath79_reset_rr(AR933X_RESET_REG_BOOTSTRAP);
 	t |= AR933X_BOOTSTRAP_MDIO_GPIO_EN;
 	ath79_reset_wr(AR933X_RESET_REG_BOOTSTRAP, t);
         
 	// Put the avr reset to high 
-	if (gpio_request_one(DS_GPIO_AVR_RESET,
+	if (gpio_request_one(DS_GPIO_AVR_RESET_DS2,
                  GPIOF_OUT_INIT_LOW | GPIOF_EXPORT_DIR_FIXED,
                  "OE-1") != 0)
                 printk("Error setting GPIO OE\n");
-	gpio_unexport(DS_GPIO_AVR_RESET);
-	gpio_free(DS_GPIO_AVR_RESET);
+	gpio_unexport(DS_GPIO_AVR_RESET_DS2);
+	gpio_free(DS_GPIO_AVR_RESET_DS2);
 #endif
 
 	// enable OE of level shifter
